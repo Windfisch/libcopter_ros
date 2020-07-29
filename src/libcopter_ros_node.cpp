bc8da963610700a5bf0281c6b57feb6a1cff9de1
Florian Jung <florian.jung@fau.de>
2020-07-29 14:23:10 +0200

    proper timestamp handling

--- src/libcopter_ros_node.cpp
+++ src/libcopter_ros_node.cpp
@@ -34,18 +34,33 @@ void cmd_panic_callback(const std_msgs::Empty& msg)
 	drone.panic();
 }
 
+/** converts a drone timestamp into ROS format */
+ros::Time timestamp(double drone_timestamp /* [sec] */)
+{
+	static std::optional<ros::Duration> t0 = std::nullopt;
+	static std::optional<double> t0_drone = std::nullopt;
+
+	if (!t0_drone.has_value()) t0_drone = drone_timestamp;
+	std::cout << drone_timestamp << " - " << *t0_drone << std::endl;
+	ros::Time t_raw(drone_timestamp-*t0_drone);
+
+	
+	if (!t0.has_value()) t0 = t_raw - ros::Time::now();
+	ROS_DEBUG_THROTTLE(1, "timestamp age: %5.1f ms", 1000.0*((t_raw - *t0 - ros::Time::now()).toSec()) );
+	return t_raw - *t0;
+}
+
 int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "libcopter_node");
 	ros::NodeHandle n;
+	ros::Rate loop_rate(90);
 
 	ros::Publisher image = n.advertise<sensor_msgs::Image>("camera_raw", 5);
 	ros::Subscriber cmd_vel_subscriber = n.subscribe("cmd_vel",5, cmd_vel_callback);
 	ros::Subscriber takeoff_subscriber = n.subscribe("takeoff",1, cmd_takeoff_callback);
 	ros::Subscriber land_subscriber = n.subscribe("land",1, cmd_land_callback);
 	ros::Subscriber panic_subscriber = n.subscribe("panic",1, cmd_panic_callback);
-
-	ros::Rate loop_rate(90);
 
 	cv_bridge::CvImage cv_img;
 	cv_img.encoding = "bgr8";
@@ -56,25 +71,25 @@ int main(int argc, char** argv)
 		exit(1);
 	}
 	
-	int framecnt = 0;
-	std::optional<double> t0 = std::nullopt;
 	while (ros::ok())
 	{
 		auto [video_frames, telemetry_frames] = drone.poll_data();
-		framecnt += video_frames.size();
 		if (video_frames.size() > 0)
 		{
 			auto& frame = video_frames.back();
-			cv_img.image = frame.frame;
-			cv_img.header.seq = framecnt-1;
 
-			double t = frame.timestamp;
-			if (!t0.has_value())
-				t0 = t - std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now().time_since_epoch()).count() / 1000000.0;
-			
-			cv_img.header.stamp.fromSec(t - *t0);
-			
-			image.publish(cv_img.toImageMsg());
+			if (frame.timestamp < 0)
+			{
+				ROS_WARN("got image frame with negative timestamp, ignoring!");
+			}
+			else
+			{
+				cv_img.image = frame.frame;
+				cv_img.header.frame_id = "libcopter";
+				cv_img.header.stamp = timestamp(frame.timestamp);
+				
+				image.publish(cv_img.toImageMsg());
+			}
 		}
 
 		ros::spinOnce();
